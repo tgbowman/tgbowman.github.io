dfadbd4a0cbceb8e97f0fcb7d9149f085a81f84f
Tyler Bowman <tylerbowman@Tylers-MacBook-Pro.local>
2017-10-09 11:50:20 -0500

    added resume and project

--- contact.js
+++ contact.js
@@ -1,7 +1,8 @@
 let contactArchive = {
     "email": "bowmant388@hotmail.com",
     "Twitter": "https://twitter.com/Tgbowman",
-    "Facebook": "https://www.facebook.com/tgbowman?ref=bookmarks"
+    "Facebook": "https://www.facebook.com/tgbowman?ref=bookmarks",
+    "Resume": "Tyler%20Bowman%20Resume%20May%202017.pdf"
 }
 
 const ContactArchiveString = JSON.stringify(contactArchive);
--- projects.js
+++ projects.js
@@ -5,8 +5,18 @@ let Project1 = {
     "Description": "In this project I was given an array of words and I needed to create a function that would print them out in sequence to the console log.  Each line had to increase the number of words by one.  I then had to figure out a way to add punctuation after every third word.",
     "Project Link": "https://tgbowman.github.io/overly-excited/"
 }
-
 projectArchive.push(Project1);
+
+let Project2 = {
+    "title": "Coins to Cash",
+    "Developer": "Tyler Bowman",
+    "Description": "In this project I had to create a function that would take an object filled with coin values and return the dollar amount.",
+    "Project Link": "https://tgbowman.github.io/Coins-to-Cash/"
+}
+projectArchive.push(Project2);
+
+
+
 
 const projectArchiveString = JSON.stringify(projectArchive);
 localStorage.setItem("ProjectArchive", projectArchiveString)
