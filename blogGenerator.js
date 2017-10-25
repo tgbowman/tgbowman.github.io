cb5960dc081130b5895c2f0673102b57c1d37cda
Tyler Bowman <tylerbowman@Tylers-MacBook-Pro.local>
2017-10-25 13:16:14 -0500

    added admin directory and added add blog form

--- blogGenerator.js
+++ blogGenerator.js
@@ -1,3 +1,11 @@
+const createBlogButton = document.getElementById("createBlog")
+const blogCancelButton = document.getElementById("cancelForm")
+const showFormButton = document.getElementById("showBlogForm")
+const form = document.getElementById("form")
+
+let blogArchive = JSON.parse(localStorage.getItem("BlogArchive")) || [];
+
+
 const blogIdGenerator = function*() {
     let uniqueId = 1
     while(true){
@@ -36,9 +44,69 @@ function blogObjectFactory (title, author, entry, ...tags) {
         }
 
     })
+}
+function getStorage() {
+    if(blogArchive.length > 0){blogArchive = JSON.parse(localStorage.getItem("BlogArchive"))
+    printBlogs()}
+}
 
+
+function printBlogs() {
+    let blogOut = document.getElementById("blogs")
+     
+        for (let i = 0; i < blogArchive.length; i++) {
+        let blog = blogArchive[i];
+        
+        blogOut.innerHTML += 
+            `
+            <article class="white-bg">
+                <h1>${blog.title}</h1>
+                <h2>by: ${blog.author}</h2>
+                <p>${blog.entry}</p>
+            </article>
+    
+            `
+        
+    }
 }
 
 
 
+function setStorage(){
+    const BlogArchiveString = JSON.stringify(blogArchive);
+    localStorage.setItem("BlogArchive", BlogArchiveString)
+    
+   
+    blogArchive = JSON.parse(localStorage.getItem("BlogArchive"))
+    
+  
+}
 
+function createNewBlog(){
+
+    let blogTitle = document.getElementById("blogTitle").value
+    let blogAuthor = document.getElementById("blogAuthor").value
+    let blogEntry = document.getElementById("blogEntry").value 
+    let blogTags = document.getElementById("blogTags").value
+
+    let newBlog = blogObjectFactory(blogTitle, blogAuthor, blogEntry, blogTags)
+
+    blogArchive.push(newBlog)
+    setStorage()
+  
+}
+
+function showForm() {
+    form.style.display="block"
+}
+
+function hideForm() {
+    form.style.display="none"
+}
+
+
+showFormButton.addEventListener("click", showForm) || 
+blogCancelButton.addEventListener("click", hideForm)
+createBlogButton.addEventListener("click" , createNewBlog)
+
+getStorage()
--- blog_print.js
+++ blog_print.js
@@ -1,16 +1,1 @@
-let blogOut = document.getElementById("blogs")
-
-for (let i = 0; i < storedBlogs.length; i++) {
-    let blog = storedBlogs[i];
-    
-    blogOut.innerHTML += 
-        `
-        <article class="white-bg">
-            <h1>${blog.title}</h1>
-            <h2>by: ${blog.author}</h2>
-            <p>${blog.entry}</p>
-        </article>
-
-        `
-    
-}
+getStorage()
